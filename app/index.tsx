efccb9c37a17d921c15e75cd7d54744031b69b44
The Building minds <n1010g@gmail.com>
2026-04-20 11:30:42 +0300

    Add subscription session check in RootScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
 import { AuthRouter } from '@/template';
 import { Redirect } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import * as subscriptions from '../services/subscriptions';
 
 export default function RootScreen() {
+  const [hasSubscriptionSession, setHasSubscriptionSession] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    subscriptions.getSubscriptionSession().then((session) => setHasSubscriptionSession(Boolean(session)));
+  }, []);
+
+  if (hasSubscriptionSession) {
+    return <Redirect href="/(tabs)" />;
+  }
+
   return (
     <AuthRouter loginRoute="/login">
       <Redirect href="/(tabs)" />
